3741bd2d1301f0b538f491836a31755823c80057
Ameer Mousavi <xerxes1718@gmail.com>
2021-05-03 06:01:47 +0430

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="false" md="3"> </v-col>
+    <v-col cols="12" sm="8" md="6"> </v-col>
+    <v-col cols="false" md="3"></v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Homepage",
+  components: {}
+};
+</script>
